b821c3bde882fffb31db30039d317427631682b7
vitortolentino <vitorafonso33@gmail.com>
2021-12-12 13:44:38 -0300

    feat: adding ProductService Create, Enabele and Disable functions

--- application/product_service.go
+++ application/product_service.go
@@ -12,3 +12,35 @@ func (service *ProductService) Get(id string) (ProductInterface, error) {
 
 	return product, nil
 }
+
+func (service *ProductService) Create(name string, price float32) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
+
+	_, err := product.IsValid()
+	return service.saveProduct(product, err)
+}
+
+func (service *ProductService) Enable(product ProductInterface) (ProductInterface, error) {
+	err := product.Enable()
+	return service.saveProduct(product, err)
+}
+
+func (service *ProductService) Disable(product ProductInterface) (ProductInterface, error) {
+	err := product.Disable()
+	return service.saveProduct(product, err)
+}
+
+func (service *ProductService) saveProduct(product ProductInterface, err error) (ProductInterface, error) {
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := service.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
